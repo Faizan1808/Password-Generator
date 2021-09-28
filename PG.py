b54218448b56e023d41490a609acaeac52ddc900
Faizan1808 <88032699+Faizan1808@users.noreply.github.com>
2021-09-29 01:17:06 +0530

    Add files via upload

---
+++ PG.py
@@ -0,0 +1,23 @@
+import random
+import string
+
+
+print('Hello, Welcome to password generator!')
+
+length = int(input('\nEnter the length of password: '))
+
+lower = string.ascii_lowercase
+
+upper = string.ascii_uppercase
+
+num = string.digits
+
+symbols = string.punctuation
+
+all = lower +upper + num + symbols
+temp = random.sample(all , length)
+
+password = "".join(temp)
+print(f'Random Password: {password}')
+
+
